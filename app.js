6ee832f986185d03cde42f45b87a105c181345cf
Abhinav Sardana <boltzsparky9@gmail.com>
2024-02-15 15:35:36 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,90 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const { log } = require('console')
+// const os =require('os')
+// const path = require('path')
+// const {readFile, writeFileSync} = require('fs')
+// const user = os.userInfo()
+// console.log(user)
+// console.log(os.uptime())
+// const abs = path.resolve(__dirname)
+// console.log(abs)
+// const first = readFile('./content/first.txt', 'base64url')
+// const second = readFile('./content/second.txt', 'binary')
+// console.log(first);
+// writeFileSync('./content/Third.txt' , `This is third: ${first}, ${second}`)
